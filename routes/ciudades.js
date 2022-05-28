33631eb865c32a86f843243acfd05bb1e8afb1f6
facundoalvarado9 <facundoalvarado9@gmail.com>
2022-05-28 18:16:00 -0300

    Refactor rutas ciudades

--- routes/ciudades.js
+++ routes/ciudades.js
@@ -9,11 +9,10 @@ const sucursalesRouter = express.Router({mergeParams: true})
 const ciudadesController = require('../controllers/ciudades')
 const sucursalesController = require('../controllers/sucursales')
 
-router.use('/:cod_postal/sucursales', sucursalesRouter)
-
 router.get('', ciudadesController.getAllCiudades)
 
 router.get('/:cod_postal', ciudadesController.getCiudadByCP)
+router.use('/:cod_postal/sucursales', sucursalesRouter)
 
 sucursalesRouter.get('/', sucursalesController.getSucursalesByCiudad)
 
