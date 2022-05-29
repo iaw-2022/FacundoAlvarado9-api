6fb38a7b85c0e4a537c7663a5a165dfe5a9f1acb
facundoalvarado9 <facundoalvarado9@gmail.com>
2022-05-29 16:52:43 -0300

    Filtrado por query en origenes

--- controllers/origenes.js
+++ controllers/origenes.js
@@ -9,12 +9,26 @@ const prisma = new PrismaClient({
 const sendResult = require('../utils/response/resultsSender')
 const paginationHelper = require('../utils/query/paginationHelper')
 
+function getSearchStringPrismaQuery(searchString){
+  return searchString ? {
+    OR: [
+      { nombre: { contains: searchString, mode: 'insensitive' } },
+    ]
+  } : {}
+}
+
 const getAllOrigenes = async (req, res, next) => {
   const pageSize = paginationHelper.getPageSize(req, next)
   const startIndex = paginationHelper.getStartIndex(req, next)
 
+  const searchString = req.query.searchString
+  const searchStringPrismaQuery = getSearchStringPrismaQuery(searchString)
+
   try{
       const origenes = await prisma.origenes.findMany({
+        where:{
+          ...searchStringPrismaQuery,          
+        },
         skip: startIndex,
         take: pageSize,
       })
