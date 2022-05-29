68071a2f6956e1dd3cd446fde3beeda373911daa
facundoalvarado9 <facundoalvarado9@gmail.com>
2022-05-29 16:52:51 -0300

    Filtrado por query en tostadurias

--- controllers/tostadurias.js
+++ controllers/tostadurias.js
@@ -9,12 +9,26 @@ const prisma = new PrismaClient({
 const sendResult = require('../utils/response/resultsSender')
 const paginationHelper = require('../utils/query/paginationHelper')
 
+function getSearchStringPrismaQuery(searchString){
+  return searchString ? {
+    OR: [
+      { nombre: { contains: searchString, mode: 'insensitive' } },
+    ]
+  } : {}
+}
+
 const getAllTostadurias = async (req, res, next) => {
   const pageSize = paginationHelper.getPageSize(req, next)
   const startIndex = paginationHelper.getStartIndex(req, next)
 
+  const searchString = req.query.searchString
+  const searchStringPrismaQuery = getSearchStringPrismaQuery(searchString)
+
   try{
       const tostadurias = await prisma.tostadurias.findMany({
+        where: {
+          ...searchStringPrismaQuery,          
+        },
         skip: startIndex,
         take: pageSize,
       })
