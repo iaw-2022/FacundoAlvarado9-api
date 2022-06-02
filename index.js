8540c662abad606ad1b3f012c49ee46169f78e51
facundoalvarado9 <facundoalvarado9@gmail.com>
2022-06-02 16:42:00 -0300

    Se agrega rate limiter + comentarios

--- index.js
+++ index.js
@@ -7,17 +7,29 @@ const prisma = new PrismaClient()
 
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./docs/swagger/swagger.yaml');
+const swaggerDocument = YAML.load('./docs/swagger/swagger.yaml'); //Locación del documento swagger
+
+const rateLimit = require('express-rate-limit')
+
+const limiter = rateLimit({
+	windowMs: 10 * 60 * 1000, // Ventana: 10 minutos
+	max: 600, // Máximo de 600 requests por cada 10 minutos por ventana de tiempo
+	standardHeaders: true,
+	legacyHeaders: false,
+})
 
 const ciudadesRoute = require('./routes/ciudades')
 const variedadesRoute = require('./routes/variedades')
 const tostaduriasRoute = require('./routes/tostadurias')
 const origenesRoute = require('./routes/origenes')
 
+app.use(limiter)
+
 app.use('/ciudades', ciudadesRoute)
 app.use('/variedades', variedadesRoute)
 app.use('/tostadurias', tostaduriasRoute)
 app.use('/origenes', origenesRoute)
+
 
 app.listen(
     process.env.PORT || PORT,
@@ -26,17 +38,19 @@ app.listen(
     })
 
 app.get('/', (req, res)=>{
-    res.send("Primera prueba para la api.")
+    res.redirect('/api-docs')
 })
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+//Ante una request ajena a las rutas reconocidas, se devuelve un 404
 app.use((req, res, next) => {
     const error = new Error('No encontrado')
     error.status = 404
     next(error)
 })
 
+//Ante otros errores no contemplados, se devuelve un 500
 app.use((error, req, res, next) =>{
   res.status(error.status || 500)
   res.json({
@@ -46,6 +60,7 @@ app.use((error, req, res, next) =>{
   })
 })
 
+//Necesaria para el manejo de BigInts (tipo de los IDs de los objetos)
 BigInt.prototype.toJSON = function() {
      return Number(this)
 }
